bb275f9d65c6b04e198536ea3afcf8e69554458e
Nileshp4321 <86780342+Nileshp4321@users.noreply.github.com>
2023-09-02 11:39:58 +0530

    Add files via upload

---
+++ src/Components/Footer/FooterR.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const FooterR = () => {
+  return (
+    <>
+        <div className='mt-8 w-100 h-96 bg-gray-800'>
+         <div className='cotainer mx-auto m-2 '>
+            <div>
+                <h1 className='text-center text-2xl mt-16 text-gray-100'>Ready To Design Your Dream Home?</h1>
+            </div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default FooterR
---
+++ src/Components/Vistit/Visit.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Visit = () => {
+    return (
+        <>
+            <div className='p-3 text-gray-100 w-full h-96 bg-gradient-to-r  from-teal-400 via-teal-500 to-teal-600'>
+                <div className='flex justify-center'><h1 className='text-2xl'>Visit Our Nearest Experience Centre</h1></div>
+                <div className='flex relative overflow-x-scroll flex-wrap '>
+                    <button type="button" className="m-2 p-5 text-white bg-blue-700 hover:bg-gray-100 hover:text-blue-500 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Whitefield, Bengaluru</button>
+                    <button type="button" className="m-2 p-5 text-white bg-blue-700 hover:bg-gray-100 hover:text-blue-500 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">HSR Layout, Bengaluru</button>
+                    <button type="button" className="m-2 p-5 text-white bg-blue-700 hover:bg-gray-100 hover:text-blue-500 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Thane, Mumbai</button>
+                    <button type="button" className="m-2 p-5 text-white bg-blue-700 hover:bg-gray-100 hover:text-blue-500 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Navi Mumbai</button>
+                    <button type="button" className="m-2 p-5 text-white bg-blue-700 hover:bg-gray-100 hover:text-blue-500 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Mount Road, Chennai</button>
+                    <button type="button" className="m-2 p-5 text-white bg-blue-700 hover:bg-gray-100 hover:text-blue-500 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">OMR, Chennai</button>
+                    <button type="button" className="m-2 p-5 text-white bg-blue-700 hover:bg-gray-100 hover:text-blue-500 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">MG Road, Bengaluru</button>
+                    <button type="button" className="m-2 p-5 text-white bg-blue-700 hover:bg-gray-100 hover:text-blue-500 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">MG Road, Bengaluru</button>
+                    <button type="button" className="m-2 p-5 text-white bg-blue-700 hover:bg-gray-100 hover:text-blue-500 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">MG Road, Bengaluru</button>
+                    <button type="button" className="m-2 p-5 text-white bg-blue-700 hover:bg-gray-100 hover:text-blue-500 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">MG Road, Bengaluru</button>
+                    <button type="button" className="m-2 p-5 text-white bg-blue-700 hover:bg-gray-100 hover:text-blue-500 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">MG Road, Bengaluru</button>
+                </div>
+                <div className='container mx-auto mt-5'>
+                    
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Visit
